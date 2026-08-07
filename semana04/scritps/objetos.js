15275e5cf5156fec7759e5070660f49f20f5d257
rochadevbyu <rferreiradarocha@byupathway.edu>
2026-08-07 19:55:42 -0300

    atiividades semana 04

---
+++ semana04/scritps/objetos.js
@@ -0,0 +1,10 @@
+ let umCurso = {
+          codigo: "WDD131",
+          nome: "Fundamentos da Web Dinâmica",
+          creditos: 2,
+          secoes: [
+            {scao:"001", matriculado: 95, instrutor: "Professor A"},
+            {scao:"002", matriculado: 80, instrutor: "Professor B"},
+            {scao:"003", matriculado: 100, instrutor: "Professor C"}
+          ]
+        };
